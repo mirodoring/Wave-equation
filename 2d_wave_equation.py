f943bad21b964bbfab20af3727efb890836064d0
mirodoring <105181702+mirodoring@users.noreply.github.com>
2022-05-09 10:08:56 -0700

    Update 2d_wave_equation.py

--- 2d_wave_equation.py
+++ 2d_wave_equation.py
@@ -10,23 +10,22 @@ Created on Fri May  6 14:52:06 2021
 import numpy as np
 import matplotlib.pyplot as plt
 #%%
-c = 343        #Velocidade de propagação
-xmax = 300      #Tamanho do eixo x
-nx = 500        #número de amostras do eixo x
-zmax = 300      #Tamanho do eixo z
-nz = 500        #Número de amostras do eixo z
-dx = 1          #Passo do eixo x
-dz = dx         #Passo do eixo z
-isrx = int(xmax/2)      #Localização da fonte no eixo x
-isrz = int(zmax/2)      #Localização da fonte no eixo z
+c = 343         #Speed of propagation
+xmax = 300      #size of x-axis
+nx = 500        #size of samples of x
+zmax = 300      #size of z-axis
+nz = 500        #size of samples of z
+dx = 1          #size of step in x
+dz = dx         #size of step in z
+isrx = int(xmax/2)      #Location of source in x axis
+isrz = int(zmax/2)      #Location of source in z axis
 
-f0 = 20         #Frequência dominante da fonte
-t0 = 1./f0      #mudança de tempo na fonte
-nt = 1000        #número de amostras em t
+f0 = 20         #Dominant frequency
+t0 = 1./f0      #Source time shift
+nt = 1000       #size of samples of t
 dt =  0.001
 
-print('Frequencia da fonte: ', f0, 'Hz')
-#Determinando o critério de estabilidade CFL
+#Determining the CFL criterion
 eps  = c * dt / dx # epsilon value
 print('Critério de estabilidade =', eps)
 
@@ -43,8 +42,8 @@ src = -8. * (t_s - t0) * f0 * (np.exp(-1.0 * (4 * f0)**2 * (t_s-t0)**2))
 
 #%%
 
-spec = np.fft.fft(src) # Função temporal da fonte no domínio da frequencia
-freq = np.fft.fftfreq(spec.size, d = dt) # domínio do tempo na frequencia
+spec = np.fft.fft(src) # Source time function in the frequency domain
+freq = np.fft.fftfreq(spec.size, d = dt) # Time domain in frequency
 plt.plot(np.abs(freq), np.abs(spec)) # plot frequency and amplitude
 plt.xlim([0,250])
 
@@ -62,22 +61,25 @@ d2pz = np.zeros((nx, nz))
 for it in range(nt):
     for i in range (1, nx-1):
         d2px[i, :] = (p[i + 1, :] - 2 * p[i, :] + p[i - 1, :]) / dx ** 2
+        #Second pressure derivative in respect to x
         
     for j in range (1, nz-1):
         d2pz[:, j] = (p[:, j + 1] - 2 * p[:, j] + p[:, j - 1]) / dz ** 2
+        #Second pressure derivative in respect to z
     
     pnew = (dt ** 2) * (c ** 2) * (d2px + d2pz) + 2 * p - pold
+    #Calculation of the presure field
     
     # pnew[isrc] = pnew[isrc] + dt**2 * src[it]
     # pnew[isrx, isrz] = pnew[isrx, isrz] + src[it] * dt ** 2
     pnew[isrz, isrx] = pnew[isrz, isrx] + src[it] / (dx * dz) * (dt ** 2) 
+    #Again calculating the pressure field but with loop for time
     
-    pold, p = p, pnew
+    pold, p = p, pnew #Assigning the variables for an effective loop
 
 plt.figure()
 plt.title("2D Wave equation")
 plt.imshow(pnew)
-# plt.clim(-1e05,1e05)
 plt.colorbar()
 plt.show()
 
