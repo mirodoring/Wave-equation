1c970e8bbc8057201a90bcf9ac1c287d18952705
mirodoring <105181702+mirodoring@users.noreply.github.com>
2022-05-11 16:46:02 -0700

    Update 2D_wave_heterogeneous.py

--- 2D_wave_heterogeneous.py
+++ 2D_wave_heterogeneous.py
@@ -30,7 +30,7 @@ isrx = 100      #Source location at x-axis
 isrz = 100      #Source location at z-axis
 snap = 200      #Snapshot frequency
 t0 = 999./f0
-
+oper == 3
 #Satisfying the CFL criteria:
 
 dx = (c0/f0)/20 #dividing velocity by frequency and an arbitrary number of grid points to image each wavelet
@@ -103,12 +103,23 @@ for it in range(nt):
     #Using a 3 point operator for the second derivative
 
     for i in range (1, nx-1):
-        d2px[i, :] = (p[i + 1, :] - 2 * p[i, :] + p[i - 1, :]) / dx ** 2
+        d2px[i, :] = (p[i + 1, :] - 2 * p[i, :] + p[i - 1, :])
         
     for j in range (1, nz-1):
-        d2pz[:, j] = (p[:, j + 1] - 2 * p[:, j] + p[:, j - 1]) / dz ** 2
+        d2pz[:, j] = (p[:, j + 1] - 2 * p[:, j] + p[:, j - 1])
+       
+    #5 point operator for the finite-difference
+    if oper == 5:
+        for i in range (2, nx-2):
+            d2px[i, :] = -1/12*p[i + 2, :] + 4/3*p[i + 1, :] \
+                          -5/2 * p[i, :] + 4/3*p[i - 1, :] + -1/12 * p[i - 2, :]
+                
+        for j in range (2, nz-2):
+            d2pz[:, j] = -1/12*p[:, j + 2] + 4/3 * p[:, j + 1] \
+                          -5/2 * p[:, j] + 4/3 * p[:, j - 1] + -1/12 * p[:, j - 2]
 
-    
+    d2px /= dx ** 2
+    d2pz /= dz ** 2
     pnew = (dt ** 2) * (c ** 2) * (d2px + d2pz) + 2 * p - pold
     
     pnew[isrz, isrx] = pnew[isrz, isrx] + w[it] / (dx * dz) * (dt ** 2)
@@ -118,12 +129,13 @@ for it in range(nt):
     #     plt.figure()
     #     plt.imshow(pnew)
     #     plt.colorbar()
+    #     plt.clim(-0.2,0.2)
     
     pold, p = p, pnew
 
 plt.figure()
 plt.title("2D Wave equation")
 plt.imshow(pnew)
-# plt.clim(-1e05,1e05)
+plt.clim(-0.2,0.2)
 plt.colorbar()
 plt.show()
